32ef4712aac17c63f5c8232ae5865066c49b4164
Theharishpatel <harishpatel.tech@gmail.com>
2026-07-19 19:10:32 +0530

    chore: setup project architecture and design foundation

---
+++ src/providers/ThemeProvider.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import { ThemeProvider as NextThemeProvider } from 'next-themes'
+import type { ThemeProviderProps } from 'next-themes'
+export const ThemeProvider = ({
+  children,
+  ...props
+}: ThemeProviderProps) => {
+  return (
+    <NextThemeProvider 
+    attribute="class"
+    defaultTheme="dark"
+    enableSystem
+    disableTransitionOnChange
+    {...props}
+    >
+      {children}
+    </NextThemeProvider>
+  )
+}
